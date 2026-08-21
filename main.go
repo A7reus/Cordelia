6e55f75e9dfb3ae38fca29c4459ef708adf8a2fb
A7reus <an1ndya@proton.me>
2026-08-21 21:27:01 +0600

    add probe command (http client)

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/A7reus/Cordelia/internal/identity"
 )
+
+const defaultPort = "47777"
 
 func main() {
 	id, err := identity.Load()
@@ -16,7 +22,56 @@ func main() {
 	if err != nil {
 		log.Fatal("cordelia:", err)
 	}
-	log.Printf("Running as %s [%s]\n", id.Name, id.Fingerprint)
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "probe":
+			if len(os.Args) != 3 {
+				log.Fatal("usage: cordelia probe <host:port>")
+			}
+			probe(os.Args[2])
+			return
+		default:
+			log.Fatalf("unknown command %q", os.Args[1])
+		}
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /info", infoHandler(id))
+
+	addr := ":" + defaultPort
+	log.Printf("Serving API on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func probe(addr string) {
+	client := http.Client{Timeout: 3 * time.Second}
+
+	res, err := client.Get(fmt.Sprintf("http://%s/info", addr))
+	if err != nil {
+		log.Fatalf("probe %s: %v", addr, err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("probe %s: got status %s", addr, res.Status)
+	}
+
+	var peer identity.Identity
+	if err := json.NewDecoder(res.Body).Decode(&peer); err != nil {
+		log.Fatalf("probe %s: bad response: %v", addr, err)
+	}
+
+	log.Printf("Found peer %s [%s]", peer.Name, peer.Fingerprint)
+}
+
+func infoHandler(id identity.Identity) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(id)
+	}
 }
 
 func createIdentity() (identity.Identity, error) {
